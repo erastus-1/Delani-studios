d31ed937dce2c67e69f63abbbc33f0282cea98e3
erastus <erastuskariuki15@gmail.com>
2020-10-09 13:48:31 +0300

    add changes

--- java/scripts.js
+++ java/scripts.js
@@ -1,2 +1,9 @@
-$(document).ready();
- $()
+// var userName = document.getElementById(name).Value
+// var userEmail = document.getElementById(email).Value
+//     submit === output
+
+$(document).ready(function){
+    $("img").click(function) {
+     $("#design-icon").show();
+    };
+};
