16bcb2f9ab88b194fee16ce008455b66ebf69e12
erastus <erastuskariuki15@gmail.com>
2020-10-09 15:40:47 +0300

    add toggle

--- java/scripts.js
+++ java/scripts.js
@@ -1,37 +1,44 @@
 $(document).ready(function() {
    $("#design-icon").click(function(){
-     $("#design-icon").show();
-     $("#design-p").hide();
+     $("#design-icon").toggle();
+     $("#design-p").toggle();
  });
 
-  $("#design-icon").click(function(){
-     $("design-icon").show();
-     $("design-p").hide();
+  $("#design-p").click(function(){
+     $("design-p").toggle();
+     $("design-icon").toggle();
  });
   $("#development-icon").click(function(){
-      $("#development-icon").show();
-      $("#development-p").hide();
+      $("#development-icon").toggle();
+      $("#development-p").toggle();
   });
+  $("#development-p").click(function(){
+    $("#development-p").toggle();
+    $("#development-icon").toggle();
+});
   $("#management-icon").click(function(){
-      $("#management-icon").show();
-      $("#management-p").hide();
+      $("#management-icon").toggle();
+      $("#management-p").toggle();
   });
-
+  $("#management-p").click(function(){
+    $("#management-p").toggle();
+    $("#management-icon").toggle();
+});
   
 });
 
-$(document).ready(function() {
-    $("#myform").submit(function(event) {
-      var myform = ["development-icon", "development-p", "management-icon", "management-p", "design-icon", "design-p"];
+// $(document).ready(function() {
+//     $("#myform").submit(function(event) {
+//       var myform = ["development-icon", "development-p", "management-icon", "management-p", "design-icon", "design-p"];
 
-      myform.forEach(function(myform) {
-        var userInput = $("input#" + myform).val();
-        $("." + myform).text(userInput);
-      });
+//       myform.forEach(function(myform) {
+//         var userInput = $("input#" + myform).val();
+//         $("." + myform).text(userInput);
+//       });
 
-      $("#submit").show();
+//       $("#submit").show();
 
-      event.preventDefault();
-    });
-  });
+//       event.preventDefault();
+//     });
+//   });
 
